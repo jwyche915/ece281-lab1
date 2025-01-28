4b9fdd2e3e24e8752213bdf0dce2b4da04bbdb83
jwyche915 <191650353+jwyche915@users.noreply.github.com>
2025-01-27 17:16:55 -0700

    Lab 1 completed

--- src/thirtyOneDayMonth.vhd
+++ src/thirtyOneDayMonth.vhd
@@ -56,10 +56,11 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
-	
-	
-	
-						-- output
+	i_B : in std_logic;
+	i_C : in std_logic;
+	i_D : in std_logic;
+		
+	o_Y : out std_logic -- output
   );
 end thirtyOneDayMonth;
 
@@ -67,13 +68,24 @@ architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is
 	-- include components declarations and signals
 	
 	--signals internal to the architecture are declared and initialized such as w_sel
+	signal w_sel : std_logic_vector (2 downto 0);  -- MUX sel
   
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
-	w_sel(0) <= i_C;	-- one
-	--finish assigning signals
+	w_sel(0) <= i_C;   -- connect input C to LSB of w_sel
+	w_sel(1) <= i_B;   -- connect input B to bit 1 of w_sel
+	w_sel(2) <= i_A;   -- connect input A to MSB of w_sel
 	
-	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.
+	o_Y <= i_D when (w_sel = "000") else
+	       i_D when (w_sel = "001") else
+	       i_D when (w_sel = "010") else
+	       i_D when (w_sel = "011") else
+	       NOT i_D when (w_sel = "100") else
+	       NOT i_D when (w_sel = "101") else
+	       NOT i_D when (w_sel = "110") else
+	       NOT i_D when (w_sel = "111") else
+	       '0';	
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
